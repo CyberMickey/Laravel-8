a36bb5153b61601cf743cf590442e99a697473fd
Cyber Mickey <mickey@cybermickey.com>
2022-05-09 12:01:34 +0200

    Store blog posts as HTML files

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,15 +6,15 @@
     </head>
     <body>
         <article>
-            <h1 ><a href="/post">First Post</a> </h1>
+            <h1 ><a href="/post/first-post">First Post</a> </h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, illo, porro? Dolor, dolores explicabo facilis illum, inventore itaque iusto laboriosam laudantium nulla quas quia, sapiente suscipit tempora ullam ut vero.</p>
         </article>
         <article>
-            <h1 ><a href="/post">Second Post</a></h1>
+            <h1 ><a href="/post/second-post">Second Post</a></h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, illo, porro? Dolor, dolores explicabo facilis illum, inventore itaque iusto laboriosam laudantium nulla quas quia, sapiente suscipit tempora ullam ut vero.</p>
         </article>
       <article>
-          <h1 ><a href="/post">Third Post</a></h1>
+          <h1 ><a href="/post/third-post">Third Post</a></h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, illo, porro? Dolor, dolores explicabo facilis illum, inventore itaque iusto laboriosam laudantium nulla quas quia, sapiente suscipit tempora ullam ut vero.</p>
         </article>
 
