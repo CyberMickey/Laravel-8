408720cffd14fdccdf7a2bbd639adf201ca64ece
Cyber Mickey <mickey@cybermickey.com>
2022-05-09 11:12:12 +0200

    Include app.css. Make a route and link to it

---
+++ resources/views/post.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Blog </title>
+        <link rel="stylesheet" href="/app.css">
+    </head>
+    <body>
+        <article>
+            <h1>First Post</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, illo, porro? Dolor, dolores explicabo facilis illum, inventore itaque iusto laboriosam laudantium nulla quas quia, sapiente suscipit tempora ullam ut vero.</p>
+            <a href="/">Back home</a>
+        </article>
+    </body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Blog </title>
+        <link rel="stylesheet" href="/app.css">
+    </head>
+    <body>
+        <article>
+            <h1 ><a href="/post">First Post</a> </h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, illo, porro? Dolor, dolores explicabo facilis illum, inventore itaque iusto laboriosam laudantium nulla quas quia, sapiente suscipit tempora ullam ut vero.</p>
+        </article>
+        <article>
+            <h1 ><a href="/post">Second Post</a></h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, illo, porro? Dolor, dolores explicabo facilis illum, inventore itaque iusto laboriosam laudantium nulla quas quia, sapiente suscipit tempora ullam ut vero.</p>
+        </article>
+      <article>
+          <h1 ><a href="/post">Third Post</a></h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, illo, porro? Dolor, dolores explicabo facilis illum, inventore itaque iusto laboriosam laudantium nulla quas quia, sapiente suscipit tempora ullam ut vero.</p>
+        </article>
+
+    </body>
+</html>
